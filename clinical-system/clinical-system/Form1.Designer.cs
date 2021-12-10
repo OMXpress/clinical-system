35d26e40e706c82242af8e7bf81c7f77141d8587
Omar Khaled <57796344+O-Okasha@users.noreply.github.com>
2021-12-10 18:18:39 +0200

    Add files via upload

---
+++ clinical-system/clinical-system/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace clinical_system
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_AddApp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Control;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(158, 121);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1251, 640);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btn_AddApp
+            // 
+            this.btn_AddApp.Location = new System.Drawing.Point(1279, 55);
+            this.btn_AddApp.Name = "btn_AddApp";
+            this.btn_AddApp.Size = new System.Drawing.Size(130, 30);
+            this.btn_AddApp.TabIndex = 1;
+            this.btn_AddApp.Text = "Add Appointment";
+            this.btn_AddApp.UseVisualStyleBackColor = true;
+            this.btn_AddApp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1421, 773);
+            this.Controls.Add(this.btn_AddApp);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn_AddApp;
+    }
+}
+
